1ddf161e735e85dcabcc0ae2bbe40bf750ce5af5
olga <olga.bylnova@expert-soft.net>
2023-11-25 15:32:12 +0300

    Localize attribute values using impEx

--- bin/custom/concerttours/src/concerttours/setup/ConcerttoursCustomSetup.java
+++ bin/custom/concerttours/src/concerttours/setup/ConcerttoursCustomSetup.java
@@ -36,6 +36,8 @@ public class ConcerttoursCustomSetup
         LOG.info("Starting custom project data loading for the Concerttours extension");
         impexImport("/impex/concerttours-bands.impex");
         impexImport("/impex/concerttours-yBandTour.impex");
+        impexImport("/impex/concerttours-bands-en.impex");
+        impexImport("/impex/concerttours-bands-de.impex");
         LOG.info("Custom project data loading for the Concerttours extension completed.");
         return true;
     }
